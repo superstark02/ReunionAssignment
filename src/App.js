ef8a0ae7be65ca2edc58cb1034651edaddd7901e
superstark01 <superstark02@gmail.com>
2022-08-31 23:32:59 +0530

    data left

--- src/App.js
+++ src/App.js
@@ -5,34 +5,26 @@ import HeaderBar from "./components/Header/HeaderBar";
 import PropertyCard from "./components/PropertyCard/PropertyCard";
 import SearchComp from "./components/SearchComp/SearchComp";
 import { data } from "./utils/DummyData";
+import "./App.css"
 
 function App() {
   const [value, setValue] = useState(data);
-  // Function to handle change in Name
-  const handleName = (e, val) => {
-    var arr;
-    if (val === null || val === '') {
-      arr = data;
-    }
-    else
-      arr = data.filter((dt) => dt.name === val.name);
-    setValue(arr);
-  }
-  // Function to implement Filtering
+  
   const handleFilter = (location, date, priceRange, type) => {
     const dt = new Date(date);
     var arr = data.filter((val) => {
-      const x = new Date(val.moveInDate);
-      if (val.location === location && x >= dt && val.price >= priceRange.lp && val.price <= priceRange.up && val.propertyType === type)
+      const x = new Date(val.date);
+      if (val.city === location && x >= dt && val.price >= priceRange.lp && val.price <= priceRange.up && val.propertyType === type)
         return true;
       return false;
     })
     setValue(arr);
   }
-  //Function To reset all filters
+  
   const handleReset = () => {
     setValue(data);
   }
+
   return (
     <div className="App">
       <div>
@@ -40,19 +32,17 @@ function App() {
       </div>
       <div>
         <Container maxWidth='lg' sx={{ py: 20 }}>
-          <SearchComp handler={handleName} />
+          <SearchComp />
           <FilterSearchBar handler={handleFilter} reset={handleReset} />
           <Box >
-            <Grid container spacing={6} justifyContent='center' >
+            <div className="grid" >
               {value.map((dt, i) => {
                 return <Grid key={i} item xs={true} >
                   <PropertyCard data={dt} />
                 </Grid>
               })}
-
-            </Grid>
+            </div>
           </Box>
-
         </Container>
       </div>
     </div>
--- src/components/PropertyCard/PropertyCard.js
+++ src/components/PropertyCard/PropertyCard.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Divider, Stack } from '@mui/material';
+import { Divider } from '@mui/material';
 import { Box } from '@mui/system';
 import BedIcon from '@mui/icons-material/Bed';
 import BathtubIcon from '@mui/icons-material/Bathtub';
@@ -23,19 +23,26 @@ const PropertyCard = ({ data }) => {
         />
         <PopTag />
         <div className='card-content' >
-          <div className='card-price' >
-            ${data.price}<span style={{ fontSize: 15, color: 'grey' }}>/month</span>
+          <div style={{display:"flex", justifyContent:'space-between'}} >
+            <div>
+              <div className='card-price' >
+                ${data.price}<span style={{ fontSize: 15, color: 'grey' }}>/month</span>
+              </div>
+              <Typography fontWeight='bold' variant='h5' >
+                {data.name}
+              </Typography>
+              <Box sx={{ paddingY: '0.6rem' }}>
+                <Typography variant="body2" color="text.secondary" >
+                  {data.address}
+                </Typography>
+              </Box>
+            </div>
+            <div>
+              <FavoriteBorderIcon sx={{ color: '#7c4dff' }} />
+            </div>
           </div>
-          <Typography fontWeight='bold' variant='h5' >
-            {data.name}
-          </Typography>
-          <Box sx={{ paddingY: '0.6rem' }}>
-            <Typography variant="body2" color="text.secondary" >
-              {data.address}
-            </Typography>
-          </Box>
           <Divider flexItem />
-          <div style={{display:"flex",justifyContent:"space-between", alignItems:"center",marginTop:"10px"}}>
+          <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "10px" }}>
             <BedIcon sx={{ color: '#7c4dff' }} />
             <Typography fontSize={13}>{data.beds} Beds</Typography>
             <BathtubIcon sx={{ color: '#7c4dff' }} />
@@ -53,7 +60,7 @@ const PropertyCard = ({ data }) => {
 const PopTag = () => {
   return <Box position='absolute' sx={{ top: "55%", transform: "translate(-8px,-100%)" }}>
     <Box sx={theme => ({ background: '#7c4dff', p: 1, borderRadius: 1, borderBottomLeftRadius: 0, padding: '5px 10px' })}>
-      <Typography sx={{ color: 'white',fontSize: '12px' }}> <StarIcon sx={{fontSize:"15px"}} /> Popular</Typography>
+      <Typography sx={{ color: 'white', fontSize: '12px' }}> <StarIcon sx={{ fontSize: "15px" }} /> Popular</Typography>
     </Box>
     <Box sx={{
       width: 0, height: 0,
